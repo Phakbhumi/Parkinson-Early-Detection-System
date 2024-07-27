d3d4bf231aa18fc1d6b249d9dadcb0cbf34dbe48
backibomi <kphakbhumi@gmail.com>
2024-07-27 09:16:49 +0700

    Minor bug fix

--- lib/src/check-up/spiral/display_spiral.dart
+++ lib/src/check-up/spiral/display_spiral.dart
@@ -5,7 +5,7 @@ import 'package:gap/gap.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:gal/gal.dart';
 
-class ViewDrawing extends StatelessWidget {
+class ViewDrawing extends StatefulWidget {
   const ViewDrawing({
     super.key,
     required this.capturedImage,
@@ -17,12 +17,32 @@ class ViewDrawing extends StatelessWidget {
   final bool isParkinson;
   final int index;
 
+  @override
+  State<ViewDrawing> createState() => _ViewDrawingState();
+}
+
+class _ViewDrawingState extends State<ViewDrawing> {
+  int downloadState = 3; // 0 - not download, 1 - start download, 2 - finish download, 3 - error
+
   Future<void> saveImage() async {
-    final tempDir = await getTemporaryDirectory();
-    String filePath = '${tempDir.path}/spiral-${DateTime.now().toString()}.png';
-    File file = await File(filePath).create();
-    await file.writeAsBytes(capturedImage);
-    await Gal.putImage(filePath);
+    if (downloadState != 0) return;
+    try {
+      setState(() {
+        downloadState = 1;
+      });
+      final tempDir = await getTemporaryDirectory();
+      String filePath = '${tempDir.path}/spiral-${DateTime.now().toString()}.png';
+      File file = await File(filePath).create();
+      await file.writeAsBytes(widget.capturedImage);
+      await Gal.putImage(filePath);
+      setState(() {
+        downloadState = 2;
+      });
+    } catch (_) {
+      setState(() {
+        downloadState = 3;
+      });
+    }
   }
 
   @override
@@ -39,7 +59,7 @@ class ViewDrawing extends StatelessWidget {
             ),
           ),
           child: Image.memory(
-            capturedImage,
+            widget.capturedImage,
             fit: BoxFit.cover,
           ),
         ),
@@ -54,32 +74,94 @@ class ViewDrawing extends StatelessWidget {
         const Gap(10),
         FilledButton(
           onPressed: saveImage,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(
-                Icons.download,
+          child: downloadStateDisplay(),
+        ),
+        if (widget.index != 2) const Gap(20),
+      ],
+    );
+  }
+
+  Widget downloadStateDisplay() {
+    switch (downloadState) {
+      case 0:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.download,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: 20,
+            ),
+            const Gap(8),
+            Text(
+              "ดาวน์โหลดรูป",
+              style: TextStyle(
+                fontSize: 16,
                 color: Theme.of(context).colorScheme.onPrimary,
-                size: 20,
               ),
-              const Gap(8),
-              Text(
-                "ดาวน์โหลดรูป",
+            ),
+          ],
+        );
+      case 1:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const CircularProgressIndicator(),
+            const Gap(8),
+            Text(
+              "กำลังโหลดรูป",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        );
+      case 2:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.check,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: 20,
+            ),
+            const Gap(8),
+            Text(
+              "ดาวน์โหลดเสร็จสิ้น",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        );
+      default:
+        return Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              Icons.error,
+              color: Theme.of(context).colorScheme.onPrimary,
+              size: 20,
+            ),
+            const Gap(8),
+            Expanded(
+              child: Text(
+                "เกิดข้อผิิดพลาดบางอย่างขึ้น กรุณาลองใหม่อีกครั้ง",
                 style: TextStyle(
                   fontSize: 16,
                   color: Theme.of(context).colorScheme.onPrimary,
                 ),
               ),
-            ],
-          ),
-        ),
-        if (index != 2) const Gap(20),
-      ],
-    );
+            ),
+          ],
+        );
+    }
   }
 
   String verdictClassify() {
-    if (isParkinson == true) {
+    if (widget.isParkinson == true) {
       return "เสี่ยง";
     } else {
       return "ไม่เสี่ยง";
